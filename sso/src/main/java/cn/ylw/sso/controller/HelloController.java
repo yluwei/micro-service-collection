9f7c0961aa2710e35e490da5eaff601f5a9ac861
yanluwei <yanluwei@ghostcloud.cn>
2021-03-25 16:07:15 +0800

    测试获取ip

--- sso/src/main/java/cn/ylw/sso/controller/HelloController.java
+++ sso/src/main/java/cn/ylw/sso/controller/HelloController.java
@@ -38,35 +38,36 @@ public class HelloController {
         String ipAddress = null;
         try {
             ipAddress = request.getHeader("x-forwarded-for");
-            log.info("x-forwarded-for:", ipAddress);
+            log.info("x-forwarded-for{}:", ipAddress);
             if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
                 ipAddress = request.getHeader("Proxy-Client-IP");
-                log.info("Proxy-Client-IP:", ipAddress);
+                log.info("Proxy-Client-IP{}:", ipAddress);
             }
             if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
                 ipAddress = request.getHeader("WL-Proxy-Client-IP");
-                log.info("WL-Proxy-Client-IP:", ipAddress);
+                log.info("WL-Proxy-Client-IP{}:", ipAddress);
             }
             if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
                 ipAddress = request.getRemoteAddr();
+                log.info("RemoteIP{}:", ipAddress);
                 if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
                     // 根据网卡取本机配置的IP
                     InetAddress inet = null;
                     try {
                         inet = InetAddress.getLocalHost();
-                        log.info("本机Host：", inet);
+                        log.info("本机Host{}：", inet);
                     } catch (UnknownHostException e) {
                         e.printStackTrace();
                     }
                     ipAddress = inet.getHostAddress();
-                    log.info("本机ip：", ipAddress);
+                    log.info("本机ip{}：", ipAddress);
                 }
             }
             // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
             if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
                 if (ipAddress.indexOf(",") > 0) {
                     ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
-                    log.info("多个代理分隔后ip：", ipAddress);
+                    log.info("多个代理分隔后ip{}：", ipAddress);
                 }
             }
         } catch (Exception e) {
