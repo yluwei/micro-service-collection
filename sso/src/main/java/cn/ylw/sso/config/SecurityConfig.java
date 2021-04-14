f72513310c19d7964b2c8ecd8e5d797a9e80cb8d
yanluwei <yanluwei@ghostcloud.cn>
2021-04-14 17:48:43 +0800

    实现自动登录

--- sso/src/main/java/cn/ylw/sso/config/SecurityConfig.java
+++ sso/src/main/java/cn/ylw/sso/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package cn.ylw.sso.config;
 import cn.ylw.sso.dao.UserDao;
 import cn.ylw.sso.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +12,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,16 +36,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository repository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        // 自动创建表
+//        jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
+
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            User user = userDao.findByUsername(username);
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(user.getName()));
+            user.setAuthorities(authorities);
+            return user;
+        };
+    }
+
     // 认证配置
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> {
-            User user = userDao.findByUsername(username);
-            List<GrantedAuthority> authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            user.setAuthorities(authorities);
-            return user;
-        })
+        auth.userDetailsService(userDetailsService())
             .passwordEncoder(new Md5PasswordEncoder());
     }
 
@@ -49,15 +70,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // 配置不需要登录的请求
         http.authorizeRequests()
-            .antMatchers("/user/**")
-            .permitAll();
-        // 配置任何请求需要登录
+            .antMatchers("/user/**").permitAll();
+
+        // 配置需要登录并且角色ROLE_ADMIN访问的路径
+        http.authorizeRequests()
+            .antMatchers("/admin/**")
+            // 此方法自动凭借ROLE_
+            .hasRole("ADMIN");
+
+        // 配置需要登录并且角色ROLE_TEST访问的路径
+        http.authorizeRequests()
+            .antMatchers("/test/**")
+            // 此方法需要写全称
+            .hasAuthority("ROLE_TEST");
+
+        // 配置其他请求需要登录即可访问
         http.authorizeRequests()
             .anyRequest()
             .authenticated()
             .and()
-            .formLogin()
-            .permitAll();
+            .formLogin().loginPage("/login")
+            .permitAll()
+            .and().logout().permitAll()
+            // 配置记住登录，通过cookie存储，默认两周。
+            // 注意：登录一定要传递参数remember-me，否则不会自动登录
+            .and().rememberMe().key("sso-token")
+            // 可设置将token储存到数据库
+            .tokenRepository(repository())
+            .tokenValiditySeconds(100);
+
+        // 支持同源嵌套iframe
+        http.csrf().disable().headers().frameOptions().sameOrigin();
     }
 
     @Override
--- sso/src/main/java/cn/ylw/sso/controller/UserController.java
+++ sso/src/main/java/cn/ylw/sso/controller/UserController.java
@@ -2,8 +2,10 @@ package cn.ylw.sso.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 获取用户
@@ -11,12 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yanluwei
  * @date 2021/4/14
  */
-@RestController
+@Controller
 public class UserController {
 
+    // 登录页
+    @RequestMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
     @GetMapping("/user/detail")
+    @ResponseBody
     public Authentication getUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication;
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @GetMapping("/admin/test")
+    public String test() {
+        return "you are permitted";
+    }
+
+    @GetMapping("/test")
+    public String testAuthority() {
+        return "you are permitted test";
     }
 }
