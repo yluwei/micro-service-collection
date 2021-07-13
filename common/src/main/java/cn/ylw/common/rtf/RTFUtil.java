61f8bbe609cf9087b62ceba822dd026468907eb4
yanluwei <yanluwei@ghostcloud.cn>
2021-07-13 16:30:24 +0800

    rtf to docx工具

--- common/src/main/java/cn/ylw/common/rtf/RTFUtil.java
+++ common/src/main/java/cn/ylw/common/rtf/RTFUtil.java
@@ -5,17 +5,17 @@ import com.aspose.words.Cell;
 import com.aspose.words.CellCollection;
 import com.aspose.words.Document;
 import com.aspose.words.ImageData;
+import com.aspose.words.Node;
 import com.aspose.words.NodeCollection;
 import com.aspose.words.NodeType;
 import com.aspose.words.Paragraph;
-import com.aspose.words.ParagraphCollection;
 import com.aspose.words.Row;
 import com.aspose.words.RowCollection;
+import com.aspose.words.Run;
 import com.aspose.words.Section;
 import com.aspose.words.SectionCollection;
 import com.aspose.words.Shape;
 import com.aspose.words.Table;
-import com.aspose.words.TableCollection;
 import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
@@ -46,79 +46,106 @@ public class RTFUtil {
 
         Document rtf = new Document("D://test.rtf");
 
+        // 获取section
         SectionCollection sections = rtf.getSections();
-        for (Section next : sections) {
-            Body body = next.getBody();
-            // 读取段落
-            ParagraphCollection paragraphs = body.getParagraphs();
-            for (Paragraph paragraph : paragraphs) {
-                String text = paragraph.getText();
-
-                // poi操作word
-                XWPFParagraph documentParagraph = document.createParagraph();
-                XWPFRun run = documentParagraph.createRun();
-                run.setText(text);
-            }
-
-            // 读取表格
-            TableCollection tables = body.getTables();
-            for (Table table : tables) {
-
-                // poi操作word
-                XWPFTable documentTable = document.createTable();
-                CTTblWidth infoTableWidth = documentTable.getCTTbl().addNewTblPr().addNewTblW();
-                infoTableWidth.setType(STTblWidth.DXA);
-                infoTableWidth.setW(BigInteger.valueOf(9072));
-
-                RowCollection rows = table.getRows();
-                int i = 0;
-                for (Row row : rows) {
-                    CellCollection cells = row.getCells();
-
-                    // poi操作word
-                    XWPFTableRow documentTableRow = documentTable.getRow(i);
-                    if (documentTableRow == null) {
-                        documentTableRow = documentTable.createRow();
-                    }
-
-
-                    int j = 0;
-                    for (Cell cell : cells) {
-                        String text = cell.getText().trim();
+        for (Section section : sections) {
+            // 获取body
+            Body body = section.getBody();
+            NodeCollection childNodes = body.getChildNodes();
+            Node[] nodes = childNodes.toArray();
+            for (Node node : nodes) {
+                // 获取节点类型
+                int nodeType = node.getNodeType();
+                switch (nodeType) {
+                    case NodeType.PARAGRAPH:
+                        Paragraph paragraph = (Paragraph) node;
+                        // 获取子节点
+                        NodeCollection paragraphChildNodes = paragraph.getChildNodes();
+                        Node[] paragraphChild = paragraphChildNodes.toArray();
 
                         // poi操作word
-                        XWPFTableCell documentTableRowCell = documentTableRow.getCell(j);
-                        if (documentTableRowCell == null) {
-                            documentTableRowCell = documentTableRow.createCell();
+                        XWPFParagraph documentParagraph = document.createParagraph();
+
+
+                        for (Node pNode : paragraphChild) {
+                            int pNodeType = pNode.getNodeType();
+                            switch (pNodeType) {
+                                case NodeType.RUN:
+                                    Run run = (Run) pNode;
+
+                                    // poi操作word
+                                    XWPFRun documentParagraphRun = documentParagraph.createRun();
+                                    documentParagraphRun.setText(run.getText());
+                                    break;
+                                case NodeType.SHAPE:
+                                    Shape shape = (Shape) pNode;
+                                    if (shape.hasImage()) {
+                                        ImageData imageData = shape.getImageData();
+
+                                        // poi操作word
+                                        XWPFParagraph p = document.createParagraph();
+                                        XWPFRun pRun = p.createRun();
+                                        pRun.addPicture(new ByteArrayInputStream(imageData.getImageBytes()),
+                                            XWPFDocument.PICTURE_TYPE_JPEG, "", Units.toEMU(100), Units.toEMU(100));
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
-                        documentTableRowCell.setText(text);
-                        j++;
-                    }
-                    i++;
+                        String paragraphText = paragraph.getText();
+
+
+                        break;
+                    case NodeType.TABLE:
+
+                        Table table = (Table) node;
+                        // poi操作word
+                        XWPFTable documentTable = document.createTable();
+                        CTTblWidth infoTableWidth = documentTable.getCTTbl().addNewTblPr().addNewTblW();
+                        infoTableWidth.setType(STTblWidth.DXA);
+                        infoTableWidth.setW(BigInteger.valueOf(9072));
+
+                        RowCollection rows = table.getRows();
+                        int i = 0;
+                        for (Row row : rows) {
+                            CellCollection cells = row.getCells();
+
+                            // poi操作word
+                            XWPFTableRow documentTableRow = documentTable.getRow(i);
+                            if (documentTableRow == null) {
+                                documentTableRow = documentTable.createRow();
+                            }
+
+
+                            int j = 0;
+                            for (Cell cell : cells) {
+                                String text = cell.getText().trim();
+
+                                // poi操作word
+                                XWPFTableCell documentTableRowCell = documentTableRow.getCell(j);
+                                if (documentTableRowCell == null) {
+                                    documentTableRowCell = documentTableRow.createCell();
+                                }
+                                documentTableRowCell.setText(text);
+                                j++;
+                            }
+                            i++;
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
-
-            // 读取图片
-            NodeCollection childNodes = next.getChildNodes(NodeType.SHAPE, true);
-            for (Shape shape : (Iterable<Shape>) childNodes) {
-                if (shape.hasImage()) {
-                    ImageData imageData = shape.getImageData();
-
-                    // poi操作word
-                    XWPFParagraph paragraph = document.createParagraph();
-                    XWPFRun run = paragraph.createRun();
-                    run.addPicture(new ByteArrayInputStream(imageData.getImageBytes()),
-                        XWPFDocument.PICTURE_TYPE_JPEG, "", Units.toEMU(100), Units.toEMU(100));
-                }
-            }
-
-            // 保存docx
-            File file = new File("D://test.docx");
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            // poi操作word
-            document.write(fileOutputStream);
-            fileOutputStream.close();
         }
+
+
+        // 保存docx
+        File file = new File("D://test.docx");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        // poi操作word
+        document.write(fileOutputStream);
+        fileOutputStream.close();
     }
 
     public static void main(String[] args) throws Exception {
